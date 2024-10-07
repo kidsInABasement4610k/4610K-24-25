3cdcb13ca602ec304509117700b0e64d9432ef2b
lyn <147214907+smoothfishvely@users.noreply.github.com>
2024-10-07 18:38:10 -0400

    Update main.cpp

--- septemberBasement/src/main.cpp
+++ septemberBasement/src/main.cpp
@@ -17,18 +17,18 @@ brain  Brain;
 controller Controller1 = controller(primary);
 
 //for 4610K robot
-motor leftFront = motor(PORT11, ratio18_1, true);
-motor rightFront = motor(PORT5, ratio18_1, false);
-motor leftMiddle = motor(PORT19, ratio18_1, true);
-motor rightMiddle = motor(PORT18, ratio18_1, false);
-motor leftBack = motor(PORT15, ratio18_1, true);
-motor rightBack = motor(PORT12, ratio18_1, false);
-digital_out pnu1 = digital_out(Brain.ThreeWirePort.A);
-digital_out pnu2 = digital_out(Brain.ThreeWirePort.B);
+motor leftFront = motor(PORT7, ratio18_1, false);
+motor rightFront = motor(PORT12, ratio18_1, true);
+motor leftMiddle = motor(PORT19, ratio18_1, false);
+motor rightMiddle = motor(PORT18, ratio18_1, true);
+motor leftBack = motor(PORT15, ratio18_1, false);
+motor rightBack = motor(PORT8, ratio18_1, true);
+digital_out mm1 = digital_out(Brain.ThreeWirePort.A);
+digital_out mm2 = digital_out(Brain.ThreeWirePort.B);
 motor intake = motor(PORT17, ratio18_1, true);
 
 
-inertial aniNertial = inertial(PORT20);
+inertial aniNertial = inertial(PORT16);
 
 extern brain Brain;
 extern controller Controller1;
@@ -48,51 +48,7 @@ int aniGle = 0;
 
 void pre_auton(void) {
   aniNertial.calibrate();
-
-}
-
-void turn(int angle) { 
-  leftFront.resetPosition();
-  rightFront.resetPosition();
-  rightMiddle.resetPosition();
-  leftMiddle.resetPosition();
-  rightBack.resetPosition();
-  leftBack.resetPosition();
-
-  aniGle = angle; 
-
-  double derivative;
-  double error = angle - (leftFront.position(degrees)+rightFront.position(degrees))/2;
-  double kp = 0.5;
-  double kpt = 1.3;
-  double kd = .4;
-  double tError = angle - aniNertial.rotation(degrees);
-
-  while(fabs(error)>3){
-        
-    double previousError=error;
-    error = angle - (leftFront.position(degrees)+rightFront.position(degrees))/2;
-    derivative = error-previousError;
-    tError = angle - aniNertial.rotation(degrees);
-
-    double power = error*kp+derivative*kd;
-    if(power>100){
-      power=100;
-    }
-    leftFront.spin(forward,power+tError*kpt,pct);
-    rightFront.spin(forward,power-tError*kpt,pct);
-    leftMiddle.spin(forward,power+tError*kpt,pct);
-    rightMiddle.spin(forward,power-tError*kpt,pct);
-    leftBack.spin(forward,power+tError*kpt,pct);
-    rightBack.spin(forward,power-tError*kpt,pct);
-    wait(10,msec);
-  }
-  rightFront.stop();
-  rightMiddle.stop();
-  rightBack.stop();
-  leftFront.stop();
-  leftBack.stop();
-  leftMiddle.stop();
+  aniNertial.resetRotation();
 }
 
 //tune kp for robot (same for right turns!)
@@ -100,12 +56,16 @@ void turn(int angle) {
 void turnL(int target){
   aniGle = target;
   double error;
+  double deriviative;
+  double kd = .2;
 
-  while(aniNertial.rotation(degrees) > aniGle){
+  while(aniNertial.rotation(degrees) > aniGle + .5){
+    double prevErr = error;
     error = abs(target - aniNertial.rotation(degrees));
-    double kp = 0.5;
+    deriviative = error - prevErr;
+    double kp = .3;
     double minSpeed = 5;
-    double speed = error * kp + minSpeed;
+    double speed = error * kp + deriviative * kd + minSpeed;
 
     leftFront.spin(reverse, speed, pct);
     rightFront.spin(fwd, speed, pct);
@@ -129,17 +89,22 @@ void turnL(int target){
   rightFront.stop();
   leftBack.stop();
   rightBack.stop();
+  wait(200, msec);
 }
 
 void turnR(int target){
   aniGle = target;
   double error;
+  double deriviative;
+  double kd = .2;
 
-  while(aniNertial.rotation(degrees) < aniGle){
+  while(aniNertial.rotation(degrees) < aniGle - .5){
+    double prevErr = error;
     error = target - aniNertial.rotation(degrees);
-    double kp = 0.5;
+    deriviative = error - prevErr;
+    double kp = .3;
     double minSpeed = 5;
-    double speed = error * kp + minSpeed;
+    double speed = error * kp + deriviative * kd + minSpeed;
     
     leftFront.spin(fwd, speed, pct);
     leftBack.spin(fwd, speed, pct);
@@ -163,21 +128,30 @@ void turnR(int target){
   leftMiddle.stop();
   rightMiddle.stop();
   rightBack.stop();
+  wait(200,msec);
 }
 
 //minimum and maximum paramaters for different scenarios
-void moveF(double target, int min, int max) { 
+void moveF(double target, int min) { 
   leftFront.resetPosition();
+  rightFront.resetPosition();
+  double deriviative;
+  double error = target - (leftFront.position(degrees)+rightFront.position(degrees))/2; 
+  double kd = .2;
 
-  while(leftFront.position(degrees) < target) {
-    double error = target - leftFront.position(degrees); 
-    double kp = .4;
-    double speed = error * kp + min; 
+  while((leftFront.position(degrees)+rightFront.position(degrees))/2 < target) {
+    double prevErr = error;
+    error = target - (leftFront.position(degrees)+rightFront.position(degrees))/2; 
+    double kp = .3;
+    deriviative = error - prevErr;
+    double speed = error * kp + deriviative * kd + min; 
+    int max = 100;
     if(speed > max){
       speed = max;
     }
     leftFront.spin(fwd, speed, pct);
     rightFront.spin(fwd, speed, pct);
+    wait(10, msec);
   }
 
   leftFront.setStopping(brake);
@@ -185,24 +159,30 @@ void moveF(double target, int min, int max) {
   leftFront.stop();
   rightFront.stop();
 
-  wait(25, msec);
-
   leftFront.setStopping(coast);
   rightFront.setStopping(coast);
+  wait(200, msec);
 }
 
-void moveR(double target, int min, int max){
+void moveR(double target, int min){
   leftFront.resetPosition();
+  rightFront.resetPosition();
+  double deriviative;
+  double error = target - (leftFront.position(degrees)+rightFront.position(degrees))/2;
+  double kd = .2;
 
-  while(leftFront.position(degrees) > -target) {
-    double error = target - leftFront.position(degrees); 
-    double kp = .6;
+  while((leftFront.position(degrees)+rightFront.position(degrees))/2 > -target) {
+    double prevErr = error;
+    error = target - (leftFront.position(degrees)+rightFront.position(degrees))/2; 
+    double kp = .3;
     double speed = error * kp + min; 
+    int max = 100;
     if(speed > max){
       speed = max;
     }
     leftFront.spin(fwd, -speed, pct);
     rightFront.spin(fwd, -speed, pct);
+    wait(10,msec);
   }
 
   leftFront.setStopping(brake);
@@ -210,16 +190,122 @@ void moveR(double target, int min, int max){
   leftFront.stop();
   rightFront.stop();
 
-  wait(25, msec);
 
   leftFront.setStopping(coast);
   rightFront.setStopping(coast);
+  wait(200, msec);
 }
 
 /*At least three (3) Scored Rings of the Alliance's color
 A minimum of two (2) Stakes on the Alliance's side of the Autonomous Line with at least (1) Ring of the Alliance's color Scored
 Neither Robot contacting / breaking the plane of the Starting Line
 At least One (1) Robot contacting the Ladder */
+
+void codingChallenge(){
+  //24 fwd, right 45, 12 rev, left 90
+  moveF(1050, 10);
+  turnL(-45);
+  moveR(700, 10);
+  turnR(45);
+}
+
+void twofivezeroninea(){
+  //from the finals at highlander summit
+  //blue right
+  moveF(1500, 10);
+  turnR(45);
+  moveF(525, 10);
+  turnR(90);
+  moveR(1500, 10);
+  turnR(180);
+}
+
+void foursixtenc(){
+  //blue left
+  //back up 2 squares
+  moveR(2100, 10);
+  //turn right 45
+  turnR(45);
+  //reverse 1/2 square
+  moveR(500, 10);
+  //forward 2 squares
+  moveF(2100, 10);
+  //reverse a little
+  moveR(100, 10);
+  //180 left
+  turnL(-90);
+  //forward 2 squares
+  moveF(2100, 10);
+  //right 75
+  turnR(-15);
+  //forward to corner
+  moveF(1700, 10);
+  //reverse a little
+  moveR(200, 10);
+  //right 180
+  turnR(165);
+  //forward across the field
+  moveF(5250, 10);
+}
+
+void oneonesixeighta(){
+  //move forward a little bit
+  moveF(200, 10);
+  //right 45 
+  turnR(45);
+  //move back a little bit
+  moveR(200, 10);
+  //forward a little bit
+  moveF(200, 10);
+  // left back to 0
+  turnL(0);
+  //forward 2 squares
+  moveF(2000, 10);
+  //turn to -90
+  turnL(-90);
+  //reverse 2 squares
+  moveR(2000, 10);
+  //turns left to -45
+  turnL(-45);
+  //forward to the bar
+  moveF(2000, 10);
+
+}
+
+void onesixtyninetyninec(){
+  //right 15
+  turnR(15);
+  //reverse 1 square
+  moveR(1000, 10);
+  //left -15
+  turnL(-15);
+  //reverse 1 square
+  moveR(1000, 10);
+  //turn right 90
+  turnR(90);
+  //forward 2 squares
+  moveF(2000, 10);
+  //right 180
+  turnR(180);
+  //forward little
+  moveF(200, 10);
+  //left 125
+  turnL(125);
+  //reverse a little
+  moveR(200, 10);
+  //back to 180
+  turnR(180);
+  //forward little
+  moveF(200, 10);
+  //left 125
+  turnL(125);
+  //reverse all the way back
+  moveR(5000, 10);
+  //a little forward
+  moveF(200, 10);
+  //left 15
+  turnL(15);
+}
 
 void autonomous(void) {
 }
@@ -236,28 +322,13 @@ void drive(){
   }
 }
 
-void codingChallenge(){
-    //24 fwd, right 45, 12 rev, left 90
-    moveF(1050, 10, 40);
-    turnL(-45);
-    moveR(700, 10, 40);
-    turnR(45);
-}
-
-void twofivezeroninea(){
-    moveF(1500, 10, 40);
-    turnR(45);
-    moveF(525, 10, 40);
-    turnR(90);
-    moveR(1500, 10, 40);
-    turnR(180);
-}
-
 void usercontrol(void) {
   while (1) {
     aniNertial.calibrate();
     wait(3, sec);
-     
+    aniNertial.resetRotation();
+    turnR(90);
+    turnL(0);
   }
 }
 
